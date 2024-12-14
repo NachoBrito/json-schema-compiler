64107ef4c4518d28b4821be5f15c9e5870b540e0
Nacho Brito <soynachobrito@gmail.com>
2024-12-14 19:39:50 +0100

    empty-repo

---
+++ src/main/java/es/nachobrito/jsonschema/compiler/App.java
@@ -0,0 +1,10 @@
+package es.nachobrito.jsonschema.compiler;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
